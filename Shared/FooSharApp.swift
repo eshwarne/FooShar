02b9979763cddd3be8951f7b09f95444d7b470ae
eshwarsundar <northeasteditsofficial@gmail.com>
2022-02-13 13:24:04 +0530

    Initial Commit

---
+++ Shared/FooSharApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooSharApp.swift
+//  Shared
+//
+//  Created by Eshwar S on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FooSharApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
